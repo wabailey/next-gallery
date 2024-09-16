fc5603e71b2ed43161588fa5ac70d9a587fcb81d
wabailey <williamarthurbailey@outlook.com>
2024-09-16 17:09:10 +0100

    Updated db and removed mock data

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - [x] Deploy on vercel
 - [x] Add environment variables for database connection
 - [x] Create basic ui with mock data
+- [x] Set up postgres database connection on Vercel
+- [x] Link database in project
+- [] Create database ui
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,17 @@
 import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/th1eKx1NOm2dAIbvN3yyL4wRCTVr1SZG0bei53Hngdk8NUFP",
-  "https://utfs.io/f/th1eKx1NOm2dE59Nm6lGlH5AOYnVMicCFaufKSDm9jEydRI8",
-  "https://utfs.io/f/th1eKx1NOm2d8meTNBwJfKbxlFUQHWmBX8TVN46gS2D9ZsIy",
-];
-
-const mockImgs = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
 
-  console.log(posts);
+  console.log(images);
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImgs.map((img) => (
-          <div key={img.id} className="w-48">
-            <img src={img.url} alt="image" />
-          </div>
+        {images.map((image) => (
+          <div key={image.id}>{image.name}</div>
         ))}
       </div>
     </main>
